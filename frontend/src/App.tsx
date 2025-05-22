0f2e21a94c5c1db77e0112cf3b800d567bc46d24
aniketsingh <aniketsingh2004@gmail.com>
2025-05-22 11:12:21 +0530

    basic setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,28 @@
+import { Button } from '@/components/ui/button'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react'
+
+
+
+const App = () => {
+  return (
+    <>
+    <div className="bg-gray-900 text-red-500">
+      Aniket's website
+    </div>
+
+        
+    <header>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
+  
+          <Button variant={"outline"}> click me!</Button>
+    </>
+  )
+}
+
+export default App
