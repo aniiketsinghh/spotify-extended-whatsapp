ed484f38a5fa9e1a8a255b1160c401372c939bf6
aniketsingh <rajputbhaishaheb@gmail.com>
2025-05-23 22:57:11 +0530

    frontend pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,20 @@
 import { Route, Routes } from 'react-router'
 import HomePage from './pages/home/HomePage'
 import AuthCallbackPage from './pages/auth-callback/AuthCallbackPage'
+import { AuthenticateWithRedirectCallback } from '@clerk/clerk-react'
+import MainLayout from './layout/MainLayout'
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage/>}/>
+        <Route path="/sso-callback" element={<AuthenticateWithRedirectCallback signUpForceRedirectUrl={"/auth-callback"} />} />
+
         <Route path="/auth-callback" element={<AuthCallbackPage/>}/>
+       <Route element={<MainLayout/>}>
+         <Route path="/" element={<HomePage/>}/>
+         </Route>
+      
       </Routes>
 
 
---
+++ frontend/src/layout/MainLayout.tsx
@@ -0,0 +1,11 @@
+import { Outlet } from 'react-router'
+
+const MainLayout = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MainLayout
